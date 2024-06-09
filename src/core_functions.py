bf29cf8a5060eac03ab78a0cb629e2df3774d052
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-06-09 15:11:39 -0300

    docstrings on core functions

--- src/core_functions.py
+++ src/core_functions.py
@@ -2,9 +2,25 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 
-# Función para construir el grafo a partir de los datos proporcionados
+
+
+
 def construir_grafo(data):
+	"""
+	Función para construir el grafo a partir de los datos proporcionados.
+
+	Args:
+		data (.json): archivo en formato json de donde extraeremos los datos.
+
+	Returns:
+		G (networkx DiGraph): Grafo dirigido creado a partir de los datos proporcionados.
+		Cada nodo representa una parada de tren con atributos como tiempo, estación, tipo y demanda.
+		Las aristas representan las conexiones entre las paradas con atributos como tipo, capacidad y costo.
+	"""
+
 	G = nx.DiGraph()
+
+	# Extraemos la informacion de los datasets
 	for viaje_id, viaje_data in data["services"].items():
 		nodo1_time = viaje_data["stops"][0]["time"]
 		nodo1_station = viaje_data["stops"][0]["station"]
@@ -13,47 +29,75 @@ def construir_grafo(data):
 		nodo1_type = viaje_data["stops"][0]["type"]
 		nodo2_type = viaje_data["stops"][1]["type"]
 		max_capacidad = data["rs_info"]["max_rs"]
-		
+
+		#Fijamos la demanda
 		demanda = math.ceil(viaje_data["demand"][0]/data["rs_info"]["capacity"])
-		
+
+		# Agregamos nodos con demanda positiva o negativa según el tipo de estación
 		if nodo1_type == "A":
-			# Agregar nodos con demanda positiva o negativa según el tipo de estación
 			G.add_node(f"{nodo1_time}_{nodo1_station}_{nodo1_type}", time=nodo1_time, station=nodo1_station, type=nodo1_type, demanda=-demanda)
 			G.add_node(f"{nodo2_time}_{nodo2_station}_{nodo2_type}", time=nodo2_time, station=nodo2_station, type=nodo2_type, demanda=demanda)
 		else:
 			G.add_node(f"{nodo1_time}_{nodo1_station}_{nodo1_type}", time=nodo1_time, station=nodo1_station, type=nodo1_type, demanda=demanda)
 			G.add_node(f"{nodo2_time}_{nodo2_station}_{nodo2_type}", time=nodo2_time, station=nodo2_station, type=nodo2_type, demanda=-demanda)
-		
+
+		# Agregamos aristas con capacidad y costo
 		if nodo1_type == "D" and nodo2_type == "A":
-			# Agregar aristas con capacidad y costo
 			G.add_edge(f"{nodo1_time}_{nodo1_station}_{nodo1_type}", f"{nodo2_time}_{nodo2_station}_{nodo2_type}", tipo="tren", capacidad=max_capacidad - demanda, costo=0)
 		elif nodo1_type == "A" and nodo2_type == "D":
 			G.add_edge(f"{nodo2_time}_{nodo2_station}_{nodo2_type}", f"{nodo1_time}_{nodo1_station}_{nodo1_type}", tipo="tren", capacidad=max_capacidad - demanda, costo=0)
-	
+
 	estaciones_nodos = {}
+	# Agrupamos los nodos por estación
 	for nodo in G.nodes:
 		estacion = G.nodes[nodo]["station"]
 		if estacion not in estaciones_nodos:
 			estaciones_nodos[estacion] = []
 		estaciones_nodos[estacion].append(nodo)
 
+	# Ordenamos los nodos por tiempo.
 	for estacion, nodos in estaciones_nodos.items():
 		nodos_ordenados = sorted(nodos, key=lambda nodo: G.nodes[nodo]["time"])
 
+		# Creamos las aristas de traspaso
 		for i in range(len(nodos_ordenados) - 1):
 			G.add_edge(nodos_ordenados[i], nodos_ordenados[i + 1], tipo="traspaso", capacidad=float("inf"), costo=0)
+
+		# Creamos las aristas de traspaso
 		G.add_edge(nodos_ordenados[-1], nodos_ordenados[0], tipo="trasnoche", capacidad=float("inf"), costo=1)
-	
+
 	return G
 
-# Función para calcular el flujo máximo y el corte mínimo
+
+
 def flujo_maximo_corte_minimo(G):
+	"""
+	Calcula el flujo máximo y el corte mínimo en un grafo dirigido utilizando el algoritmo de costo mínimo.
+
+	Parámetros:
+	- G (networkx.DiGraph): Grafo dirigido creado a partir de instancia.
+
+	Returns:
+	- flowDict (dict): Diccionario con claves: (origen y destino), y valores = flujo en cada arista.
+	"""
 	flowDict = nx.min_cost_flow(G, "demanda", "capacidad", "costo")
 
 	return flowDict
 
 
-def plotear(G: nx.Graph, flowDict: dict, a, filename):
+
+
+def plotear(G: nx.Graph, flowDict: dict, title, filename):
+	"""
+	Genera y muestra un gráfico del grafo G con los flujos calculados en flowDict.
+
+	Parámetros:
+	- G (networkx.Graph): Grafo dirigido de la red de transporte.
+	- flowDict (dict): Diccionario con los flujos en cada arista del grafo.
+	- title (int): Indicador para cambiar el título del gráfico (0 para el resultado del algoritmo, 1 para la interpretación en vagones).
+	- filename (str): Nombre del archivo de instancia utilizado.
+
+	"""
 	estaciones = set(nx.get_node_attributes(G, 'station').values())
 
 	if estaciones == {"Retiro", "Tigre"}:
@@ -61,12 +105,12 @@ def plotear(G: nx.Graph, flowDict: dict, a, filename):
 			"Retiro": "blue",
 			"Tigre": "red",
 		}
-	else: 
+	else:
 		colores_estaciones = {
-			"La Plata": "blue", 
+			"La Plata": "blue",
 			"Hospital San Juan de Dios": "red",
 		}
-	
+
 	colores_aristas = {
 		"trasnoche": "red",
 		"traspaso": "blue",
@@ -89,7 +133,7 @@ def plotear(G: nx.Graph, flowDict: dict, a, filename):
 		estaciones_nodos[estacion].append(nodo)
 
 	for estacion, nodos in estaciones_nodos.items():
-		nodos_ordenados = sorted(nodos, key=lambda nodo: G.nodes[nodo]["time"])        
+		nodos_ordenados = sorted(nodos, key=lambda nodo: G.nodes[nodo]["time"])
 		separacion_vertical = 0.5
 
 		for i, nodo in enumerate(nodos_ordenados):
@@ -117,16 +161,31 @@ def plotear(G: nx.Graph, flowDict: dict, a, filename):
 		flujo = flowDict[u][v] if u in flowDict and v in flowDict[u] else 0
 		capacidad = G.edges[(u,v)]["capacidad"]
 		edge_labels[(u, v)] = f"{flujo}/{capacidad}"
-	
+
 	node_labels = {nodo: f"{nodo} ({G.nodes[nodo]['demanda']})" for nodo in G.nodes}
 	nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=8)
-	
+
 	nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
-	if a == 0:
+	if title == 0:
 		plt.title("Devolución del algoritmo min_cost_flow")
 		plt.text(0.5, -0.1, f'Archivo: {filename}\nCon adaptación y reducción de las cotas inferiores a 0 para que el algoritmo funcione.', fontsize=10, color='gray', style='italic', ha='center', transform=plt.gca().transAxes)
 	else:
 		plt.title("Interpretación de los vagones")
 		plt.text(0.5, -0.1, f'Archivo: {filename}', fontsize=10, color='gray', style='italic', ha='center', transform=plt.gca().transAxes)
-	
+
 	plt.show()
+
+def cambios_por_reparaciones (estacion_reparacion, capacidad_limitada, G):
+	aristas_trasnoche = []
+	for u,v, in G.edges():
+		if G.edges[u,v]["tipo"] == "trasnoche":
+			aristas_trasnoche.append(((u,v), G.nodes[u]["station"])) #agrego la arista y de donde sale 
+	if estacion_reparacion == G.nodes[aristas_trasnoche[1][0][0]]["station"]: #si la estacion rota es la segunda, agrego aristas de tipo trenR como corresponda
+		G.edges[aristas_trasnoche[1][0][0],aristas_trasnoche[1][0][1]]["capacidad"] = capacidad_limitada
+		G.add_edge(aristas_trasnoche[1][0][0],aristas_trasnoche[0][0][0], tipo="trenR",capacidad=float("inf"),costo=0)
+		G.add_edge(aristas_trasnoche[0][0][1],aristas_trasnoche[1][0][1], tipo="trenR", capacidad=float("inf"), costo=0)
+	else: #si la estacion rota es la primera, entonces poner aristas con el otro sentido que las de arriba
+		G.edges[aristas_trasnoche[0][0][0],aristas_trasnoche[0][0][1]]["capacidad"] = capacidad_limitada
+		G.add_edge(aristas_trasnoche[0][0][0],aristas_trasnoche[1][0][0], tipo="trenR",capacidad=float("inf"),costo=0)
+		G.add_edge(aristas_trasnoche[1][0][1],aristas_trasnoche[0][0][1], tipo="trenR", capacidad=float("inf"), costo=0)
+	return G
--- src/main.py
+++ src/main.py
@@ -1,33 +1,45 @@
 import json
 from core_functions import *
 
-# Función principal
 def main():
-	archivos = ["instances/new_instance.json", "instances/toy_instance.json", "instances/retiro-tigre-semana.json", "instances/test_instance_ward.json"]
-	archivos = ["instances/retiro-tigre-semana.json"]
-	for filename in archivos:
-		with open(filename) as json_file:
-			data = json.load(json_file)
+    archivos = ["instances/new_instance.json", "instances/toy_instance.json", "instances/retiro-tigre-semana.json", "instances/test_instance_ward.json"]
 
-		# Construye el grafo
-		G = construir_grafo(data)
+    # Si hay arreglos en progreso setear variable a TRUE,
+    # especifricar que estacione esta en reparacion y su limte de capacidad.
+    hay_arreglos_en_progreso = True
+    estacion_reparacion = 'Tigre'
+    capacidad_limitada = 2
 
-		# Calcula el flujo máximo y el corte mínimo
-		flowDict = flujo_maximo_corte_minimo(G)
 
-		# Plotteo exacto lo que tira min_cost_flow.
-		# El 0 o el 1 pasado como parámetro solamente cambia los títulos.
-		plotear(G, flowDict, 0, filename)
+    for filename in archivos:
+        with open(filename) as json_file:
+            data = json.load(json_file)
 
-		# Para la interpretacion, cambio los flujos para que representen los vagones.
-		for u, v in G.edges:
-			if G.edges[u, v]["tipo"] == "tren":
-				flowDict[u][v] += G.nodes[u]["demanda"]
-				G.edges[u,v]["capacidad"] = G.edges[u,v]["capacidad"] + G.nodes[u]["demanda"]
-				# Sumar la demanda del nodo receptor al flujo existente.
+        # Construye el grafo
+        G = construir_grafo(data)
 
-		# Plotteo con la representación medida en vagones.
-		plotear(G, flowDict, 1, filename)
+
+        if hay_arreglos_en_progreso:
+            cambios_por_reparaciones (estacion_reparacion, capacidad_limitada, G)
+
+
+        # Calcula el flujo máximo y el corte mínimo
+        flowDict = flujo_maximo_corte_minimo(G)
+        print(flowDict)
+
+        # Plotteo exacto lo que tira min_cost_flow.
+        # El 0 o el 1 pasado como parámetro solamente cambia los títulos.
+        plotear(G, flowDict, 0, filename)
+
+        # Para la interpretacion, cambio los flujos para que representen los vagones.
+        for u, v in G.edges:
+            if G.edges[u, v]["tipo"] == "tren":
+                flowDict[u][v] += G.nodes[u]["demanda"]
+            G.edges[u,v]["capacidad"] = G.edges[u,v]["capacidad"] + G.nodes[u]["demanda"]
+        # Sumar la demanda del nodo receptor al flujo existente.
+
+        # Plotteo con la representación medida en vagones.
+        plotear(G, flowDict, 1, filename)
 
 if __name__ == "__main__":
-	main()
+    main()
