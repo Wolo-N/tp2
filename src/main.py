17fdbb70d64312824076be0b0a65c9157c348899
victoriamarsili <victoriamarsili@gmail.com>
2024-06-09 19:49:07 -0300

    .

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import json
+from core_functions import *
+
+def main():
+    archivos = ["instances/short_instance.json","instances/new_instance.json", "instances/toy_instance.json",
+                "instances/retiro-tigre-semana.json", "instances/test_instance_ward.json"]
+
+    # Si hay arreglos en progreso setear variable a TRUE,
+    # especifricar que estacione esta en reparacion y su limte de capacidad.
+    hay_arreglos_en_progreso = False
+    estacion_en_reparacion = 'Tigre'
+    capacidad_limitada = 2
+
+
+    for filename in archivos:
+        with open(filename) as json_file:
+            data = json.load(json_file)
+
+        # Construye el grafo
+        G = construir_grafo(data)
+
+        if hay_arreglos_en_progreso:
+            cambios_por_reparaciones(estacion_en_reparacion, capacidad_limitada, G)
+
+        # Calcula el flujo máximo y el corte mínimo
+        flowDict = flujo_maximo_corte_minimo(G)
+
+        # Plotteo exacto lo que tira min_cost_flow.
+        # El 0 o el 1 pasado como parámetro solamente cambia los títulos.
+        plot(G, flowDict, 0, filename)
+
+        # Preparamos y plotteamos la representación medida en vagones.
+        interpretacion_vagones(G,flowDict)
+        plot(G, flowDict, 1, filename)
+
+if __name__ == "__main__":
+    main()
