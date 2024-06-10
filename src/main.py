68ec00deae34f30d9feaf13199b14db02f4ddf1f
victoriamarsili <victoriamarsili@gmail.com>
2024-06-10 13:40:56 -0300

    final

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import json
 from core_functions import *
 
 def main():
-    archivos = ["instances/short_instance_luli.json"]#,"instances/new_instance.json", "instances/toy_instance.json",
+    archivos = ["instances/short_instance_luli.json"]#, "instances/short_instance_2.json", "instances/short_instance.json","instances/new_instance.json", "instances/toy_instance.json",
                 #"instances/retiro-tigre-semana.json", "instances/test_instance_ward.json"]
 
     # Si hay arreglos en progreso setear variable a TRUE,
     # especifricar que estacione esta en reparacion y su limte de capacidad.
     hay_arreglos_en_progreso = False
     estacion_en_reparacion = 'Tigre'
-    capacidad_limitada = 10
+    capacidad_limitada = 2
 
 
     for filename in archivos:
@@ -24,15 +24,14 @@ def main():
 
         # Calcula el flujo máximo y el corte mínimo
         flowDict = flujo_maximo_corte_minimo(G)
-        #print (flowDict)
-        
+
         # Plotteo exacto lo que tira min_cost_flow.
         # El 0 o el 1 pasado como parámetro solamente cambia los títulos.
-        plot(G, flowDict, 0, filename)
+        plot(G, flowDict, 0, filename, 0)
 
         # Preparamos y plotteamos la representación medida en vagones.
         interpretacion_vagones(G,flowDict)
-        plot(G, flowDict, 1, filename)
+        plot(G, flowDict, 1, filename, True)
 
 if __name__ == "__main__":
     main()
